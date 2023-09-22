dff3fb3b900ed073c495720355caaed203dde940
dhd5076 <dylandunn17@icloud.com>
2023-09-22 13:46:27 -0400

    Added menu section items

--- components/menu.js
+++ components/menu.js
@@ -7,36 +7,36 @@ export default function Menu(pageProps) {
             case "specials":
                 return (
                     <>
-                        <Section title="Specials"/>
+                        <Section title="Specials" sectionItems=${menu.specials}/>
                     </>
                 )
             case "menu":
                 return (
                     <>
-                        <Section title="Tapas"/>
-                        <Section title="Sandwiches"/>
-                        <Section title="More"/>
+                        <Section title="Tapas" sectionItems=${menu.tapas}/>
+                        <Section title="Sandwiches" sectionItems=${menu.sandwiches}/>
+                        <Section title="More" sectionItems.more/>
                     </>
                 )
             case "cocktails":
                 return (
                     <>
-                        <Section title="Cocktails"/>
+                        <Section title="Cocktails" sectionItems=${menu.cocktails}/>
                     </>
                 )
             case "wine":
                 return (
                     <>
-                        <Section title="Red"/>
-                        <Section title="White"/>
-                        <Section title="Green"/>
+                        <Section title="Red" sectionItems=${menu.redWine}/>
+                        <Section title="White" sectionItems=${menu.whiteWine}/>
+                        <Section title="Green" sectionItems=${menu.greenWine}/>
                     </>
                 )
             case "beer":
                 return (
                     <>
-                        <Section title="Canned"/>
-                        <Section title="Draft"/>
+                        <Section title="Canned" sectionItems=${menu.cannedBeer}/>
+                        <Section title="Draft" sectionItems=${menu.draftBeer}/>
                     </>
                 )
         }
--- components/section.js
+++ components/section.js
@@ -5,7 +5,9 @@ export default function Section(pageProps) {
     return (
     <div className="lg:w-1/3 font-serif">
         <h1 className="text-2xl mb-8 font-semibold"> {pageProps.title} </h1>
-        <Item name="Imported Conservas" price="mkt. /tin" desc="Cockles, Octopus, Razor Clams or Scallops "/>
+	{pageProps.sectionItems.map((sectionItem, index) => (
+        	<Item name=${sectionItem.name} price=${sectionItem.price} desc=${sectionItem.desc}/>
+	))}
     </div>
     )
 }
