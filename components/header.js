7ea1a8cbec5ceea1d90a78cc623439256c720f95
Dylan Dunn <dylandunn17@icloud.com>
2023-09-26 02:32:39 -0400

    Best Practices

--- components/header.js
+++ components/header.js
@@ -23,7 +23,7 @@ export default function Header() {
 
   return (
     <>
-      <div id="navbar" className="backdrop-blur border-b-2 border-slate-500 max-w-7xl w-full bg-black/75 h-16 md:h-24 flex shadow-2xl shadow-black overflow-visible fixed z-50 justify-content-between">
+      <div id="navbar" className="backdrop-blur border-b-2 border-slate-500 max-w-7xl w-full bg-black/75 h-16 md:h-24 flex shadow-2xl shadow-black fixed z-50 justify-content-between">
         <div className="w-1/5 sm:w-2/5 bg-gradient-to-r from-slate-900 flex items-center">
         <button className="text-white text-xl font-semibold pl-8 visible md:invisible w-auto sm:w-0" onClick={toggleMenu}>
   {isMenuOpen ? (
@@ -37,7 +37,7 @@ export default function Header() {
           </a>
         </div>
         <div className="w-32 min-w-32 md:h-48 h-32 border-2 mx-auto md:w-48 backdrop-blur -mt-2 bg-black/100 border-slate-500 rounded-b-full shadow-2xl shadow-black ">
-            <img alt="La Bola Logo" src="/logo.png" className="object-cover md:mt-8 mt-4 w-20 md:w-32 relative mx-auto overflow-visible"></img>
+            <img alt="La Bola Logo" src="/logo.png" className="object-cover md:mt-8 mt-4 w-20 md:w-32 relative mx-auto"></img>
         </div>
         <div className="flex items-center w-1/5 sm:w-2/5 bg-gradient-to-r from-black/0 to-black">
           <div className={`space-x-4 w-full font-semibold mr-8 font-thin text-slate-100 text-right pl-4 invisible md:visible`}>
