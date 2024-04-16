7825fd2d491103af3e67d32eac4bacad7d5ad80d
ryntab <57367794+ryntab@users.noreply.github.com>
2024-04-16 00:04:42 -0400

    refactor: :recycle: prepend $ sign

--- components/Item.js
+++ components/Item.js
@@ -3,7 +3,7 @@ export default function Item(pageProps) {
         <div className="mr-2">
             <span className="flex justify-between border-b-2 border-dotted border-black">
                 <h2 className="font-semibold uppercase"> {pageProps.name}</h2>
-                <b className="ml-2 font-bold lowercase"> {pageProps.price} </b>
+                <b className="ml-2 font-bold lowercase"> ${pageProps.price} </b>
             </span>
             <p className="mb-4 mt-2 italic text-sm"> {pageProps.desc} </p>
         </div>
